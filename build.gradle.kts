ab31fb627d42588a877c09a74173997f64e8a500
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-01-03 10:55:15 +0000

    NA: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
   kotlin("plugin.spring") version "1.5.30"
   kotlin("plugin.jpa") version "1.5.30"
 }
@@ -14,21 +14,21 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
 
-  implementation("com.oracle.database.jdbc:ojdbc10:19.12.0.0")
+  implementation("com.oracle.database.jdbc:ojdbc10:19.13.0.0.1")
 
-  runtimeOnly("org.hsqldb:hsqldb:2.5.1")
-  runtimeOnly("org.flywaydb:flyway-core:7.14.0")
+  runtimeOnly("org.hsqldb:hsqldb:2.6.1")
+  runtimeOnly("org.flywaydb:flyway-core:8.3.0")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.12")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.12")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.12")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.3")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.3")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.3")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
@@ -36,7 +36,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito:mockito-inline:4.0.0")
+  testImplementation("org.mockito:mockito-inline:4.2.0")
 }
 
 java {
