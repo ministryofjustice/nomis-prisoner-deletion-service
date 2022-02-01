8604ed9c3c70f4b721465bb2c6de25d752ed8683
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-02-01 11:37:21 +0000

    GDPR-333: Update dependencies & spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.2"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
@@ -14,25 +14,25 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.5")
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
 
   implementation("com.oracle.database.jdbc:ojdbc10:19.13.0.0.1")
 
   runtimeOnly("org.hsqldb:hsqldb:2.6.1")
-  runtimeOnly("org.flywaydb:flyway-core:8.4.1")
+  runtimeOnly("org.flywaydb:flyway-core:8.4.3")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.testcontainers:localstack:1.16.2")
+  testImplementation("org.testcontainers:localstack:1.16.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
