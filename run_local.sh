a55759538e2383e08bfc9654ff3a294b841cd89d
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-08-02 15:05:19 +0100

    GDPR-346: 🔧 Add local docker-compose file & local run script

---
+++ run_local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+println () {
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+  echo $1
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+  }
+
+healthyStatus='"sqs": "running"'
+
+println "BUILDING PROJECT"
+./gradlew --no-daemon assemble
+
+println "STARTING UP DEPENDENCIES"
+docker compose -f docker-compose.local.yml up -d
+
+while [[ $(curl -sb -H "Accept: application/json" "http://localhost:4566/health") != *"$healthyStatus"* ]]; do sleep 1; done
+
+println "ALL SERVICES ARE RUNNING"
+
+println "STARTING NOMIS PRISONER DELETION SERVICE WITH THE 'DEV' PROFILE"
+./gradlew bootRun --args='--spring.profiles.active=dev'
