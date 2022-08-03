cabf246dbc5ef6e41fc2c28b3fed6182978b1cf6
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-08-03 15:36:49 +0100

    GDPR-346:⬆ Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.3"
-  kotlin("plugin.spring") version "1.7.0"
-  kotlin("plugin.jpa") version "1.7.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1"
+  kotlin("plugin.spring") version "1.7.10"
+  kotlin("plugin.jpa") version "1.7.10"
 }
 
 configurations {
@@ -14,13 +14,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.8")
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
 
   implementation("com.oracle.database.jdbc:ojdbc10:19.15.0.0.1")
 
-  runtimeOnly("org.hsqldb:hsqldb:2.6.1")
+  runtimeOnly("org.hsqldb:hsqldb:2.7.0")
   runtimeOnly("org.flywaydb:flyway-core")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
@@ -30,7 +30,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.1")
+  developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.2")
 
   testImplementation("org.testcontainers:localstack:1.17.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
