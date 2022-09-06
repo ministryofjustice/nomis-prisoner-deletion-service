68d1102929078b5dbafd6b374b0f56d7be443335
Hyder Khan <hyder.khan@digital.justice.gov.uk>
2022-09-06 11:10:38 +0100

    GDPR-346: ⬆️ Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
 }
@@ -18,7 +18,7 @@ dependencies {
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("org.apache.commons:commons-text:1.9")
 
-  implementation("com.oracle.database.jdbc:ojdbc10:19.15.0.0.1")
+  implementation("com.oracle.database.jdbc:ojdbc10:19.16.0.0")
 
   runtimeOnly("org.hsqldb:hsqldb:2.7.0")
   runtimeOnly("org.flywaydb:flyway-core")
@@ -26,9 +26,9 @@ dependencies {
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.10")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.10")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.10")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.3")
 
